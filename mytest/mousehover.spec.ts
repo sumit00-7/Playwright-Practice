51387d3b4ea8be14405a9cf94a26fba248658339
Sumit Bhatt <sbhatt.pwn@gmail.com>
2025-12-20 00:17:48 +0530

    Practice Mouse Hovering

--- mytest/mousehover.spec.ts
+++ mytest/mousehover.spec.ts
@@ -4,10 +4,16 @@ test('move to element test', async()=>{
     const browser:Browser = await chromium.launch({headless: false, channel: 'chrome'});
     const page:Page = await browser.newPage();
 
-    await page.goto("https://www.spicejet.com/");
-    await page.getByText('Add-ons').first().hover()
+    // await page.goto("https://www.spicejet.com/");
+    // await page.getByText('Add-ons').first().hover()
     // await page.getByText('Taxi').first().click()
-    await page.getByText('Visa Services').first().click()
+    // await page.getByText('Visa Services').first().click()
+
+    await page.goto("https://www.bigbasket.com/",  {waitUntil: 'domcontentloaded'})
+    await page.getByRole('button', { name: 'Shop by Category' }).last().click();
+    await page.getByText('Beverages').hover()
+    await page.getByText('Fruit Juices & Drinks').hover()
+    await page.getByRole('link', {name:'Juices'}).click()
 
 
     await page.waitForTimeout(5000)
